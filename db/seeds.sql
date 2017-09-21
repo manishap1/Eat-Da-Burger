8676b9105d0166e9bc69f294277973bbf5f862bc
manishap1 <manishapawar.net@gmail.com>
2017-09-21 16:25:19 -0500

    files added

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hot n spicy burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('fish burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Simply Burger', true);
